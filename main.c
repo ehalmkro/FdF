a0cd8ad34792755b735fc6d6ed44da41b4cd98fb
Esa Halmkrona <ehalmkro@student.hive.fi>
2019-12-17 17:09:40 +0200

    TODO added LOL

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ehalmkro <ehalmkro@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/06 11:45:49 by ehalmkro          #+#    #+#             */
-/*   Updated: 2019/12/08 17:21:28 by ehalmkro         ###   ########.fr       */
+/*   Updated: 2019/12/17 17:05:23 by ehalmkro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,13 @@ int		ft_read(char *str, char **ret)
 
 	if ((fd = open(str, O_RDONLY)) == -1)
 		return (-1);
+/*	TODO:
+**	READ INPUT LINE BY LINE AND CHECK THE INPUT: SHOULD ONLY CONTAIN NUMBERS AND EVERYLINE SHOULD HAVE THE SAME
+**	LENGTH. AFTER VALIDATING THE INPUT FORM AN INTEGER ARRAY OUT OF IT (WITH ATOI)
+*/
+
 	while ((get_next_line(fd, line)) == 1)
-	{
-	
+    {
 
 	}
 
