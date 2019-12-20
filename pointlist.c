7f5265cad71f6041d03cdfc5e81a15c0dac772c3
Esa Halmkrona <ehalmkro@student.hive.fi>
2019-12-20 20:18:47 +0200

    Update read: reading works, lots of leaks

--- pointlist.c
+++ pointlist.c
@@ -6,7 +6,7 @@
 /*   By: esko <esko@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/19 13:29:54 by esko              #+#    #+#             */
-/*   Updated: 2019/12/19 14:50:48 by esko             ###   ########.fr       */
+/*   Updated: 2019/12/20 19:50:24 by esko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,7 @@ t_point     *point_node_new(int x, int y, int z)
 {
     t_point *ret;
 
-    ret = malloc(sizeof(t_point));
-    ret->x = (int)malloc(sizeof(int));
-    ret->y = (int)malloc(sizeof(int));
-    ret->z = (int)malloc(sizeof(int));
-    ret->next = malloc(sizeof(t_point*));
+    ret = (t_point*)malloc(sizeof(t_point));
     ret->x = x;
     ret->y = y;
     ret->z = z;
@@ -30,8 +26,11 @@ t_point     *point_node_new(int x, int y, int z)
 
 void        point_node_pushback(t_point **start, t_point *new)
 {
-    while ((*start)->next)
-        *start = (*start)->next;
-    (*start)->next = new;
+    t_point *current;
+
+    current = *start;
+    while (current->next)
+        current = current->next;
+    current->next = new;
 
 }
