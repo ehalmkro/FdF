1133a939560c7a23ecba60e7cfcbd4ca93078e0c
Esa Halmkrona <ehalmkro@student.hive.fi>
2020-01-14 16:02:50 +0200

    add ideaj files

---
+++ cmake-build-debug/CMakeFiles/FdF.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FdF.dir/main.c.o"
+  "CMakeFiles/FdF.dir/pointlist.c.o"
+  "CMakeFiles/FdF.dir/draw_window.c.o"
+  "CMakeFiles/FdF.dir/tools.c.o"
+  "FdF.pdb"
+  "FdF"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FdF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
