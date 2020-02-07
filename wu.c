494dfeb79cab89ea0a9bf03271be796dfdebfa1f
Esa Halmkrona <ehalmkro@student.hive.fi>
2020-02-07 12:56:52 +0200

    wu refactor before norminnette

--- wu.c
+++ wu.c
@@ -6,7 +6,7 @@
 /*   By: ehalmkro <ehalmkro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 19:31:14 by ehalmkro          #+#    #+#             */
-/*   Updated: 2020/02/07 11:13:44 by ehalmkro         ###   ########.fr       */
+/*   Updated: 2020/02/07 12:55:52 by ehalmkro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,13 @@
 
 static void	swap_float(float *a, float *b)
 {
-	float temp;
+	float	temp;
 
 	temp = *a;
 	*a = *b;
 	*b = temp;
 }
+
 static void	swap_height(t_height *a, t_height *b)
 {
 	t_height temp;
@@ -54,12 +55,15 @@ static void	endpoint_swap(t_point *start, t_point *end, t_scene *draw)
 		swap_height(&start->height, &end->height);
 	}
 }
-void 		draw_line_wu(t_point start, t_point end, t_scene *draw)
-{
-	/*t_point		*delta;
-	t_point		*current;
-	t_point		*current_2;
 
+void		draw_line_wu(t_point start, t_point end, t_scene *draw)
+{
+	t_point		*delta;
+	t_map		*curr_pxl;
+	t_map		*start_end;
+
+	start_end = map_add_node(&start);
+	start_end->nxt = map_add_node(&end);
 	draw->steep = fabs(end.y - start.y) > fabs(end.x - start.x) ? 1 : 0;
 	if (draw->steep == 1 || start.x > end.x)
 		endpoint_swap(&start, &end, draw);
@@ -70,53 +74,57 @@ void 		draw_line_wu(t_point start, t_point end, t_scene *draw)
 	int xend = round(start.x);
 	int yend = start.y + gradient * (xend - start.x);
 	double xgap = RFPART_(start.x + 0.5);
-	current = point_node_new(xend, IPART_(yend), 0, draw);
+	curr_pxl = map_add_node(point_node_new(xend, IPART_(yend), 0, draw));
 	if (draw->steep == 1)
 	{
-		put_pixel(current->y, current->x,  decrease_brightness(set_color(start, end, *delta, *current, *draw), RFPART_(yend) * xgap), draw);
-		put_pixel(current->y + 1, current->x, decrease_brightness(set_color(start, end, *delta, *current, *draw), FPART_(yend) * xgap), draw);
+		put_pixel(curr_pxl->dt->y, curr_pxl->dt->x,  decrease_brightness(set_color(start_end, *delta, *curr_pxl->dt, *draw), RFPART_(yend) * xgap), draw);
+		put_pixel(curr_pxl->dt->y + 1, curr_pxl->dt->x, decrease_brightness(set_color(start_end, *delta, *curr_pxl->dt, *draw), FPART_(yend) * xgap), draw);
 	}
 	else
 	{
-		put_pixel(current->x, current->y, decrease_brightness(set_color(start, end, *delta, *current, *draw), RFPART_(yend) * xgap), draw);
-		put_pixel(current->x, current->y + 1, decrease_brightness(set_color(start, end, *delta, *current, *draw), FPART_(yend) * xgap), draw);
+		put_pixel(curr_pxl->dt->x, curr_pxl->dt->y, decrease_brightness(set_color(start_end, *delta, *curr_pxl->dt, *draw), RFPART_(yend) * xgap), draw);
+		put_pixel(curr_pxl->dt->x, curr_pxl->dt->y + 1, decrease_brightness(set_color(start_end, *delta, *curr_pxl->dt, *draw), FPART_(yend) * xgap), draw);
 	}
 	double intery = yend + gradient;
 	xend = round(end.x);
 	yend = end.y + gradient * (xend - end.x);
 	xgap = FPART_(end.x + 0.5);
-	current_2 = point_node_new(xend, IPART_(yend), 0, draw);
+	curr_pxl->nxt = map_add_node(point_node_new(xend, IPART_(yend), 0, draw));
 	if (draw->steep == 1)
 	{
-		put_pixel(current_2->y, current_2->x,  decrease_brightness(set_color(start, end, *delta, *current, *draw), RFPART_(yend) * xgap), draw);
-		put_pixel(current_2->y + 1, current_2->x, decrease_brightness(set_color(start, end, *delta, *current, *draw), FPART_(yend) * xgap), draw);
+		put_pixel(curr_pxl->nxt->dt->y, curr_pxl->nxt->dt->x,  decrease_brightness(set_color(start_end, *delta, *curr_pxl->dt, *draw), RFPART_(yend) * xgap), draw);
+		put_pixel(curr_pxl->nxt->dt->y + 1, curr_pxl->nxt->dt->x, decrease_brightness(set_color(start_end, *delta, *curr_pxl->dt, *draw), FPART_(yend) * xgap), draw);
 	}
 	else
 	{
-		put_pixel(current_2->x, current_2->y, decrease_brightness(set_color(start, end, *delta, *current, *draw), RFPART_(yend) * xgap), draw);
-		put_pixel(current_2->x, current_2->y + 1, decrease_brightness(set_color(start, end, *delta, *current, *draw), FPART_(yend) * xgap), draw);
+		put_pixel(curr_pxl->nxt->dt->x, curr_pxl->nxt->dt->y, decrease_brightness(set_color(start_end, *delta, *curr_pxl->dt, *draw), RFPART_(yend) * xgap), draw);
+		put_pixel(curr_pxl->nxt->dt->x, curr_pxl->nxt->dt->y + 1, decrease_brightness(set_color(start_end, *delta, *curr_pxl->dt, *draw), FPART_(yend) * xgap), draw);
 	}
 	if (draw->steep == 1)
 	{
-		while (current->x < current_2->x)
+		while (curr_pxl->dt->x < curr_pxl->nxt->dt->x)
 		{
-			put_pixel(IPART_(intery), current->x, decrease_brightness(set_color(start, end, *delta, *current, *draw), RFPART_(intery)), draw);
-			put_pixel(IPART_(intery) + 1, current->x, decrease_brightness(set_color(start, end, *delta, *current, *draw), FPART_(intery)), draw);
+			put_pixel(IPART_(intery), curr_pxl->dt->x, decrease_brightness(set_color(start_end, *delta, *curr_pxl->dt, *draw), RFPART_(intery)), draw);
+			put_pixel(IPART_(intery) + 1, curr_pxl->dt->x, decrease_brightness(set_color(start_end, *delta, *curr_pxl->dt, *draw), FPART_(intery)), draw);
 			intery += gradient;
-			current->x++;
+			curr_pxl->dt->x++;
 		}
 	}
 	else
 	{
-		while (current->x < current_2->x)
+		while (curr_pxl->dt->x < curr_pxl->nxt->dt->x)
 		{
-			put_pixel(current->x, IPART_(intery), decrease_brightness(set_color(start, end, *delta, *current, *draw), RFPART_(intery)), draw);
-			put_pixel(current->x, IPART_(intery) + 1, decrease_brightness(set_color(start, end, *delta, *current, *draw), FPART_(intery)), draw);
+			put_pixel(curr_pxl->dt->x, IPART_(intery), decrease_brightness(set_color(start_end, *delta, *curr_pxl->dt, *draw), RFPART_(intery)), draw);
+			put_pixel(curr_pxl->dt->x, IPART_(intery) + 1, decrease_brightness(set_color(start_end, *delta, *curr_pxl->dt, *draw), FPART_(intery)), draw);
 			intery += gradient;
-			current->x++;
+			curr_pxl->dt->x++;
 		}
 	}
 	free(delta);
-	free(current);
-	free(current_2);*/
+	free(curr_pxl->dt);
+	free(curr_pxl->nxt->dt);
+	free(curr_pxl->nxt);
+	free(curr_pxl);
+	free(start_end->nxt);
+	free(start_end);
 }
