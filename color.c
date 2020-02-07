7c3448f6d903ff937b9a156da25a073f28391d6d
Esa Halmkrona <ehalmkro@student.hive.fi>
2020-02-07 12:19:42 +0200

    norme fix

--- color.c
+++ color.c
@@ -6,7 +6,7 @@
 /*   By: ehalmkro <ehalmkro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 18:05:47 by ehalmkro          #+#    #+#             */
-/*   Updated: 2020/02/07 11:19:07 by ehalmkro         ###   ########.fr       */
+/*   Updated: 2020/02/07 12:18:42 by ehalmkro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		*split_color(int color)
 {
 	int *ret;
 
-	ret = malloc(sizeof(int*) * 3);
+	(ret = (int*)malloc(sizeof(int*) * 3)) == NULL ? ft_error(0) : 0;
 	ret[2] = color & 255;
 	color >>= 8;
 	ret[1] = color & 255;
@@ -58,15 +58,17 @@ int		set_color(t_map *start_end, t_point delta, t_point current,\
 	int		color[2];
 
 	start_end->dt->height == HI ? color[0] = draw.color[2] : 0;
-	start_end->dt->height== ZERO ? color[0] = draw.color[1] : 0;
+	start_end->dt->height == ZERO ? color[0] = draw.color[1] : 0;
 	start_end->dt->height == LO ? color[0] = draw.color[0] : 0;
 	start_end->nxt->dt->height == HI ? color[1] = draw.color[2] : 0;
 	start_end->nxt->dt->height == ZERO ? color[1] = draw.color[1] : 0;
 	start_end->nxt->dt->height == LO ? color[1] = draw.color[0] : 0;
 	if (delta.x > delta.y)
-		pct = get_percent((int)start_end->dt->x, (int)start_end->nxt->dt->x, (int)current.x);
+		pct = get_percent((int)start_end->dt->x, (int)start_end->nxt->dt->x, \
+		(int)current.x);
 	else
-		pct = get_percent((int)start_end->dt->y, (int)start_end->nxt->dt->y, (int)current.y);
+		pct = get_percent((int)start_end->dt->y, (int)start_end->nxt->dt->y, \
+		(int)current.y);
 	r = get_color_point((color[0] >> 16) & 0xFF, (color[1] >> 16) & 0xFF, pct);
 	g = get_color_point((color[0] >> 8) & 0xFF, (color[1] >> 8) & 0xFF, pct);
 	b = get_color_point(color[0] & 0xFF, color[1] & 0xFF, pct);
